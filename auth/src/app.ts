95203599d9dc6f5cd24d621c93d46589c1b573cb
unknown <fashina.ayodimeji@cloudfift.com>
2021-11-28 10:37:44 +0100

    Install cors

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -28,8 +28,7 @@ var cors = require('cors');
 
 import cookieSession from 'cookie-session';
 const app = express();
-// app.use(cors({origin: '*'}));
-app.use('*', cors())
+app.use(cors({origin: '*'}));
 app.set('trust proxy', true);
 app.use(json());
 
